a01918106d4c24b3ce947f5e0b4af0509d1b5ad3
lucas-paz <lucas.hpaz@outlook.com>
2021-10-26 17:46:16 -0300

    feat: req 1

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
@@ -9,6 +11,8 @@ const io = require('socket.io')(http, {
 });
 
 require('./sockets/chatSocket')(io);
+
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
