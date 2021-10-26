e783486e5ebac48b7a548052cf04dc8004a4854a
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-10-26 16:23:58 -0300

    Feat: Inicia projeto webChat

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+io.on('connection', () => { // socket
+  console.log('Alguen se conectou');
+});
+
+// app.get('/teste', (req, res) => {
+//   res.sendFile(`${__dirname}/index.html`);
+// });
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
