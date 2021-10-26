33b1478998cf90bd4b01c9a8b138c46d0b0c4783
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-26 15:58:00 -0300

    Socket.io server side initial setup

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const http = require('http');
 const dotenv = require('dotenv');
+const { Server } = require('socket.io');
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+const ioOptions = {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+};
+
+const io = new Server(server, ioOptions);
+
+io.on('connection', (socket) => {
+  console.log('New connection. Id: ' + socket.id);
+});
 
 app.get('/test', (_req, res) => {
   res.status(200).send('OK');
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+server.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
 });
