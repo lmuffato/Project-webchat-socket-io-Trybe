67e64ee0ae6201a89a23a5f8382a071bf70f35b7
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-26 16:24:55 -0300

    fix: message event emitting format

--- sockets/message.js
+++ sockets/message.js
@@ -2,8 +2,8 @@ module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log(`New connection. Id: ${socket.id}`);
 
-    socket.on('message', (message) => {
-      io.emit('message', message);
+    socket.on('message', ({ chatMessage, nickname }) => {
+      io.emit('message', chatMessage);
     });
   });
 };
