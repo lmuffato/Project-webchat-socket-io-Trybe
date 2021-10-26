2acea926e5b3204b9c2e835d1f1fbd26ceeba0c2
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-26 16:18:01 -0300

    Server side socket now emit messages to all clients

--- sockets/message.js
+++ sockets/message.js
@@ -1,5 +1,9 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log(`New connection. Id: ${socket.id}`);
+
+    socket.on('message', (message) => {
+      io.emit('message', message);
+    });
   });
 };
