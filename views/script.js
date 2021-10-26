03a51a32c252116e755a5a9b30e1f8f8b0154b9d
Orlando Flores <orlando_flores@id.uff.br>
2021-10-26 18:53:12 -0300

    feat: cria logica para modificar nick e para renderizar os usuarios conectados

--- views/script.js
+++ views/script.js
@@ -3,19 +3,50 @@ const client = window.io();
 const sendBtn = document.getElementById('send-btn');
 const input = document.getElementById('message');
 const list = document.getElementById('list');
+const users = document.getElementById('users');
+const nickBtn = document.getElementById('nickname-button');
+const nickInput = document.getElementById('nickname-box');
+
+const saveNick = () => {
+  if (nickInput.value) {
+    sessionStorage.setItem('nickname', nickInput.value);
+  }
+  nickInput.value = '';
+};
+
+const getNick = () => {
+  const nickname = sessionStorage.getItem('nickname');
+  return nickname;
+};
+
+nickBtn.addEventListener('click', saveNick);
 
 const createMsg = (msg) => {
   const li = document.createElement('li');
+  li.setAttribute('data-testid', 'message');
   li.innerText = msg;
   list.append(li);
 };
 
+const createNewUser = (id) => {
+  const li = document.createElement('li');
+  li.setAttribute('data-testid', 'online-user');
+  li.innerText = id;
+  users.append(li);
+};
+
 sendBtn.addEventListener('click', (e) => {
   e.preventDefault();
+  const nickname = getNick() || client.id.substring(0, 16);
   if (input.value) {
-    const nickname = client.id;
     client.emit('message', { chatMessage: input.value, nickname });
   }
+  input.value = '';
 });
 
 client.on('message', (msg) => createMsg(msg));
+
+client.on('newUser', (allUsers) => {
+  users.innerText = '';
+  allUsers.forEach((id) => createNewUser(id));
+});
