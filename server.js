92080b21421b626cbe4d540bb14923b9bfa3b5e6
Eduardo Seije <eduardoseije@gmail.com>
2021-10-26 15:30:40 -0300

    configurações iniciais de variáveis de ambiente, express e ejs

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 // Faça seu código aqui
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
