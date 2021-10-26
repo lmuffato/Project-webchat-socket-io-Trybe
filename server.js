ded91cc7dbe31b19df7c23af8cd604dbad06f8c9
Felipe Muller <felipeavmuller@gmail.com>
2021-10-26 18:55:40 -0300

    Cria o servidor

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const server = require('http').createServer(app);
+
+const PORT = 3000;
+
+app.use(cors());
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
