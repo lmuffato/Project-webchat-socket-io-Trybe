00114923b37a458997011276df9ee6a33923dd9c
AdelinoJnr <adelinojunior96@gmail.com>
2021-10-26 10:57:50 -0300

    refactor: functions helpers

--- sockets/webchat.js
+++ sockets/webchat.js
@@ -5,16 +5,15 @@ const allUsers = {};
 const webchat = (io) => {
   io.on('connection', async (socket) => {
     socket.emit('allMessages', await Helpers.allMessages());
-
     allUsers[socket.id] = socket.id.substring(0, 16);
     io.emit('allUsers', Object.values(allUsers));
-  
-    socket.on('nickname', (data) => {
-      allUsers[socket.id] = data;
+
+    socket.on('nickname', (nickname) => {
+      allUsers[socket.id] = nickname;
       io.emit('allUsers', Object.values(allUsers));
     });
   
-    socket.on('message', async (data) => io.emit('message', await Helpers.generetorMessage(data)));
+    socket.on('message', (data) => Helpers.generetorMessage(data, io));
   
     socket.on('disconnect', () => {
       delete allUsers[socket.id];
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,12 @@
 const moment = require('moment');
 const Model = require('../models/message');
 
-const generetorMessage = async ({ chatMessage, nickname }) => {
+const allUsers = {};
+
+const generetorMessage = async ({ chatMessage, nickname }, io) => {
   const timestamp = moment().format('DD-MM-yyyy HH:mm:ss A');
   await Model.create({ message: chatMessage, nickname, timestamp });
-  return `${timestamp} - ${nickname}: ${chatMessage}`;
+  io.emit('message', `${timestamp} - ${nickname}: ${chatMessage}`);
 };
 
 const allMessages = async () => {
@@ -12,7 +14,27 @@ const allMessages = async () => {
   return messages;
 };
 
+const listUsers = async () => Object.values(allUsers);
+
+const disconnect = (socket, io) => {
+  delete allUsers[socket.id];
+  io.emit('allUsers', listUsers());
+};
+
+const addUser = (socket, nickname, io) => {
+  allUsers[socket.id] = nickname;
+  io.emit('allUsers', listUsers());
+};
+
+const connection = (socket) => {
+  allUsers[socket.id] = socket.id.substring(0, 16);
+};
+
 module.exports = {
   generetorMessage,
   allMessages,
+  listUsers,
+  disconnect,
+  addUser,
+  connection,
 };
