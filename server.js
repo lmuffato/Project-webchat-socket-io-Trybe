f1371f717d9a907edc80dbd08ba5655a093c7952
Felipe Flores <felipefloresvendas@gmail.com>
2021-10-26 14:23:23 -0300

    added public and sockts folders

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  } });
+
+  app.use(cors());
+
+  require('./sockets/chat')(io);
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
