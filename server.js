e2ea60ab2749410dbb39c50e41c2768821fda0c9
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-10-26 18:06:37 -0300

    req01: add server + view eng config

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-// Faça seu código aqui
+const express = require('express');
+const http = require('http');
+require('dotenv').config();
+
+// criando serviço
+const app = express();
+const server = http.createServer(app);
+
+// setando a view eng
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// declarando rota
+app.get('/', (_req, res) => res.render('index'));
+
+// criando conexão
+const { env: { PORT } } = process;
+const myPort = PORT || 3000;
+server.listen(myPort, () => {
+  console.log(`ouvindo na porta ${myPort}`);
+});
