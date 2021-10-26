648508307f4705d4212b36a28f15f81db75cc6d5
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-10-26 19:54:28 -0300

    Feat: evento para capturar a msg do input

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 require('dotenv').config();
+const cors = require('cors');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.render('client');
-});
 
 const io = require('socket.io')(server, {
   cros: {
@@ -20,8 +17,22 @@ const io = require('socket.io')(server, {
   },
 });
 
-io.on('connection', () => {
-  console.log('Feita a conexão! Novo usuário conectado.');
+io.on('connection', (socket) => {
+  console.log(`Feita a conexão! Novo usuário conectado ${socket.id}`);
+
+  socket.on('message', (msg) => {
+    console.log(msg);
+  });
 });
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.render('client');
+});
+
+// app.post('/', (req, res) => {
+
+// });
 
 server.listen(PORT, console.log(`Ouvindo Socket.io server na porta ${PORT}`));
