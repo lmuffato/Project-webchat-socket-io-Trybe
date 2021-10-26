0e880fee0946d60cee0d48f9324306037379667b
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-26 17:15:20 -0300

    Refactoring to separate app from server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require('express');
 const http = require('http');
 const dotenv = require('dotenv');
 const { Server } = require('socket.io');
 const message = require('./sockets/message');
+const app = require('./app');
 
 dotenv.config();
 
-const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
@@ -18,15 +17,7 @@ const ioOptions = {
 };
 
 const io = new Server(server, ioOptions);
-
 message(io);
-
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
-app.get('/test', (_req, res) => {
-  res.status(200).render('test-client');
-});
 
 server.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
