3bb04754b5713e72923c1edb9acaeecbd9ce106f
Lucas André <lucas.cancado89@gmail.com>
2021-10-26 15:55:26 -0300

    Starting inicial configs

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Faça seu código aqui
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  }
+})
+
+app.use(cors());
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+})
