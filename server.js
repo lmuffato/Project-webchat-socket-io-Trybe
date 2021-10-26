6ee51a36018cd7b7bf9d349c226cbf0d18e4b21b
lucas-paz <lucas.hpaz@outlook.com>
2021-10-26 15:53:00 -0300

    creates server as well as start a socket instance

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+require('./sockets/chatSocket')(io);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
