d88bfa3a9fe14a963845ced0cd6fd0885917a52a
Ana Ventura <anaventura1811@gmail.com>
2021-10-26 18:54:54 -0300

    feat: configurações iniciais pasta sockets e server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const corsSettings = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsSettings));
+
+app.get('/', async (req, res) => {
+  const chat = '';
+});
