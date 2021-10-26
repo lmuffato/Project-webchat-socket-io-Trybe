5bc783b6e4ddeeefa24ea8747658dcc9f505df0b
lucas-paz <lucas.hpaz@outlook.com>
2021-10-26 16:21:29 -0300

    fix: event name at broadcast

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,7 +1,7 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
     socket.on('message', (msg) => {
-      io.broadcast.emit('serverMessage', msg);
+      socket.broadcast.emit('message', msg);
     });
   });
 };
