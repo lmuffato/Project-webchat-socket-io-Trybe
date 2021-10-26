954902ce17ebb94fe760151e5b95e8489f34163b
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-10-26 20:52:05 -0300

    req01: first part of webchat done

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,6 +1,8 @@
 // arquivo criado com ajuda do guia:
 // https://app.betrybe.com/course/back-end/sockets/sockets-socketio/7eba74a2-260a-4a4d-954e-14d8cc7a9d92/conteudo/20a6c196-10d9-4db5-b20c-cfe68b3dd1d0/construindo-um-chat-com-socketio/d48e7081-f992-4a5f-ba1b-0fe28dd08967?use_case=side_bar
 
+const socket = window.io();
+
 // listando os elementos
 const msgInput = document.getElementById('msgInput');
 const sendButton = document.getElementById('sendButton');
@@ -16,6 +18,7 @@ const createMessage = (message) => {
 // add evento no sendButton
 sendButton.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log(msgInput.value);
-  return createMessage(msgInput.value);
+  socket.emit('clientMessage', msgInput.value);
+  createMessage(msgInput.value);
+  msgInput.value = ''
 });
--- sockets/webchat.js
+++ sockets/webchat.js
@@ -1,4 +1,5 @@
 module.exports = (io) => io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id} `);
   socket.on('clientMessage', (message) => {
     console.log(`Mensagem ${message}`);
     io.emit('serverMessage', message);
