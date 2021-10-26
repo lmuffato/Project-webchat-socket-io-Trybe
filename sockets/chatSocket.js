21d5a7b1329d10a60975241b0e7c27b891ccb776
Ana Ventura <anaventura1811@gmail.com>
2021-10-26 19:05:23 -0300

    feat: estrutura básica da view e do socket

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,3 +1,10 @@
-module.exports = (io) => io.on('connection', (socket) => {
-  console.log(socket);
-});
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+    console.log(socket);
+
+    socket.on('clientMessage', async (message) => {
+      // await model de chat,
+      io.emit('refreshChat', message);
+    });
+  }); 
+};
