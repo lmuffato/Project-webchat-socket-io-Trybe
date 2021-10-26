3d9c8ac4549780f5b2eaee8e88f14b4ac7f7f229
lucas-paz <lucas.hpaz@outlook.com>
2021-10-26 17:46:08 -0300

    update: add the timestamp to the broadcasted msg

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,7 +1,11 @@
+const moment = require('moment');
+
+const date = moment().format('DD-MM-YYYY HH:mm:ss A');
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.on('message', (msg) => {
-      socket.broadcast.emit('message', msg);
+    socket.on('message', ({ chatMessage, nickname }) => {
+      io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
     });
   });
 };
