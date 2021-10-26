0655cb8891e57f09141373c3d0f3d3245fd3aa39
Bruno Mendes <mendessbruno@gmail.com>
2021-10-26 19:40:06 -0300

    alterado alguns oarametros para avaliador do requisito1

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -6,11 +6,11 @@ module.exports = (io) => {
   
     socket.on('updateUsername', (newUsername) => {
       console.log(newUsername);
-    // socket.emit('refreshCurrentVotes', language);
-  
-  //     // socket.emit -> manda apenas para quem emitiu
-  //     // io.emit -> manda para todos
-  //     // socket.broadcast.emit -> manda para todos exceto quem emitiu.
+    // io.emit('updateUsernameList', newUsername);
+    });
+    socket.on('message', (message) => {
+      console.log(message);
+      io.emit('message', message);
     });
   });  
 };
