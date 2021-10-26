32e7a670f99367ba824b7b16e903414635b06bca
Viegas <mvrdgs@gmail.com>
2021-10-26 16:36:09 -0300

    configuraçao inicial do socket + requisito 1

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+require('./sockets')(io);
+
+server.listen(3000, console.log('Listening to port 3000'));
