89d2c485013c6c1c71b08c3ede3a9973a8f5cc5e
Bruno Mendes <mendessbruno@gmail.com>
2021-10-26 16:32:11 -0300

    inicializado o projeto, com a criaçao da pg, criaçao do backend e inicializaçao do socket

---
+++ controllers/chatController.js
@@ -0,0 +1,9 @@
+// // const { getJokes } = require("../models/joke");
+
+// const renderPage = (_req, res) => {
+//   res.status(200).render('interface', { joke });
+// };
+
+// module.exports = {
+//   renderPage,
+// };
--- server.js
+++ server.js
@@ -1,1 +1,39 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const ioClient = require('socket.io-client');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = require('http').createServer(app);
+
+// const chat = require('./models/Language');
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const chatSocket = require('./sockets/chatSocket');
+
+chatSocket(io);
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.set('view engine', 'ejs');
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (_req, res) => {
+  res.status(200).render('interface', { ioClient });
+});
+
+server.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
