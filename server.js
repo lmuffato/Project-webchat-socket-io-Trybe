e39b4e099cdd12eb8f92fc9307ed78662acea3ed
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-10-26 19:08:38 -0300

    Feat: primeira conexao com o socket.io

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.render('client');
+});
+
+const io = require('socket.io')(server, {
+  cros: {
+    origin: `http//localhost:${PORT}`,
+    mthods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', () => {
+  console.log('Feita a conexão! Novo usuário conectado.');
+});
+
+server.listen(PORT, console.log(`Ouvindo Socket.io server na porta ${PORT}`));
