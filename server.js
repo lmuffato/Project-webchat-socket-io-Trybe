8db66eba1d6c464200a8304f0b65ef99d4c9333f
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-26 15:38:19 -0300

    Initial commit. Iniciando o projeto #VQV :rocket:

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Faça seu código aqui
+const express = require('express');
+const http = require('http');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/test', (_req, res) => {
+  res.status(200).send('OK');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
