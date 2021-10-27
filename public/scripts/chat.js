a2ef644445d1ca4f0eac8d935bfcad1b3dfc693d
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-10-26 22:54:17 -0300

    add comments

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -2,7 +2,6 @@
 // https://app.betrybe.com/course/back-end/sockets/sockets-socketio/7eba74a2-260a-4a4d-954e-14d8cc7a9d92/conteudo/20a6c196-10d9-4db5-b20c-cfe68b3dd1d0/construindo-um-chat-com-socketio/d48e7081-f992-4a5f-ba1b-0fe28dd08967?use_case=side_bar
 
 // add socket
-
 const socket = window.io();
 
 // listando os elementos
--- sockets/webchat.js
+++ sockets/webchat.js
@@ -6,12 +6,14 @@ module.exports = (io) => io.on('connection', (socket) => {
   // lidando com as msg
   socket.on('message', (data) => {
     const { chatMessage, nickname } = data;
-    console.log(data);
 
+    // declarando a data
     const date = moment().format('DD-MM-YYYY HH:mm:ss A');
 
+    // enviando server msg for client
     io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
   });
+  
   // caso disconnect 
   socket.on('disconnect', () => {
     socket.broadcast.emit('message', `tchau ${socket.id}`);
