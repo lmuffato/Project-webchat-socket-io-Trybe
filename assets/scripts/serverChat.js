4b90ebfaead8aa36aa5f561bc5eed46138139d2b
Gabriel <g.pereira.a.m@gmail.com>
2021-10-27 11:17:29 -0300

    Revert "feat(Req 1) - :sparkle: Requisito 1 feito"
    This reverts commit 717cb4f725ebfd78e3e9e80612e2be5516bb73ca.

--- assets/scripts/serverChat.js
+++
@@ -1,8 +0,0 @@
-const moment = require('moment');
-
-module.exports = (io) => io.on('connection', (socket) => { 
-  socket.on('message', ({ chatMessage, nickname }) => {
-    const dateHours = moment().format('DD-MM-yyyy HH:mm:ss A');
-    io.emit('message', `${dateHours} - ${nickname}: ${chatMessage}`);
-  });
-});
--- controller/chatController.js
+++
@@ -1,5 +0,0 @@
-const showWebChat = (_req, res) => res.render('chat.ejs');
-
-module.exports = {
-  showWebChat,
-};
