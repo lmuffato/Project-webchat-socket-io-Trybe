b7539c8865e5cf9b16a917cde3bfbe85f282c7f0
Rafael Gerônimo <rafael@geronimo.dev>
2021-10-27 14:53:33 -0300

    feat: req1 - socketServer

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 require('dotenv/config');
 const path = require('path');
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.BACKEND_PORT || 3000;
 const server = require('http').createServer(app);
 
 const io = require('socket.io')(server, {
   cors: {
-    origin: `https://localhost:${PORT}`,
+    // origin: `http://localhost:${PORT}`,
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
 });
 
-// app.use(express.static(__dirname + '/public'));
-app.use('/', express.static('./public'));
+app.use(express.static(path.join(__dirname, '/public')));
+// app.use('/', express.static('./public'));
 
 require('./sockets/chat')(io);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public', 'index.html'));
-});
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
+
+// app.get('/', (req, res) => {
+//  res.sendFile(path.join(__dirname, './public', 'index.html'));
+// });
 
 server.listen(PORT, () => console.log(`Webchat server running on port ${PORT}!`));
