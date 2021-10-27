cf16c65bf80ccdbd3d612b0049754e8efea41de7
Gabriel Ferreira <glucas709@gmail.com>
2021-10-27 18:04:00 -0300

    fix: remove initial mesasge

--- sockets/chat.js
+++ sockets/chat.js
@@ -2,21 +2,20 @@ const generateFormatedDate = require('../utils/generateFormatedDate');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log(`${socket.username} acabou de entrar`);
     socket.on('message', ({ chatMessage, nickname = socket.username }) => {
       const date = generateFormatedDate();
       const message = `${date} ${nickname} ${chatMessage}`;
       io.emit('message', message);
     });
 
-    socket.emit('message', 'Seja bem vindo(a) ao chat!');
-
     socket.broadcast.emit('serverMessage', {
-      message: `${socket.username} acabou de entrar`,
+      message: `${socket.username} acabou de entrar.`,
     });
 
     socket.on('disconnect', () => {
-      console.log(`${socket.username} saiu`);
+      socket.broadcast.emit('serverMessage', {
+        message: `${socket.username} saiu.`,
+      });
     });
   });
 };
