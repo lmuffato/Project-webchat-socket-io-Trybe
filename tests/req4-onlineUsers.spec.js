6be127f0e0b5b0de5fb327cb88309cf660aad715
Bruno Mendes <mendessbruno@gmail.com>
2021-10-27 16:30:04 -0300

    restaurado teste desafio 4

--- tests/req4-onlineUsers.spec.js
+++ tests/req4-onlineUsers.spec.js
@@ -7,8 +7,6 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 const puppeteer = require('puppeteer');
 
-// const connection = require('../models/connection');
-
 const BASE_URL = 'http://localhost:3000/';
 
 function dataTestid(name) {
@@ -32,10 +30,6 @@ describe('4 - Informe a todos os clientes quem está online no momento', () => {
   });
 
   afterEach(async (done) => {
-    // await browser.close();
-    //   const collectionusers = await connection()// gambiarra para limpar as Mensagens junto
-    //   .then((db) => db.collection('onlineUsers'));// gambiarra para limpar as Mensagens junto
-    //   await collectionusers.deleteMany({}); // gambiarra para limpar as Mensagens junto
     done();
   });
 
