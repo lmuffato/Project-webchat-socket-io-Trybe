da0dce67d4ff96e2bba61d6d45c81650b4413a0d
Jodiel Briesemeister <jodielll29@gmail.com>
2021-10-27 19:30:44 -0300

    feat: req02

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -5,8 +5,23 @@ const hourDate = () => {
   return `${dmy} ${hourMinS}`;
 };
 
+const onlineUsers = {};
+
 module.exports = (io) => io.on('connection', async (socket) => {
+  onlineUsers[socket.id] = `Anonimo-${socket.id.slice(0, 8)}`;
+  io.emit('updateOlineList', onlineUsers);
+
   socket.on('message', async ({ chatMessage, nickname }) => {
     io.emit('message', `${hourDate()} - ${nickname}: ${chatMessage}`);
+  });
+
+  socket.on('updateName', ({ nick, id }) => { 
+    onlineUsers[id] = nick; 
+    io.emit('updateOlineList', onlineUsers);
+  });
+
+  socket.on('disconnect', () => {
+    delete onlineUsers[socket.id];
+    io.emit('updateOlineList', onlineUsers);
   });
 });
