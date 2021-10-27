b800814d2885c60473b8c4a17183bdb03fd3e5d4
Flavio Franco <flavio.win@gmail.com>
2021-10-27 14:31:22 -0300

    requisito 2

--- public/js/webChat.js
+++ public/js/webChat.js
@@ -8,14 +8,30 @@ const nickInput = document.getElementById('nickInput');
 
 let nickname = '';
 
+const addOnlineUser = (nick) => {
+  nickname = nick;
+  const spanElement = document.getElementById('user-span');
+  if (spanElement) {
+    spanElement.remove();
+  }
+  const spanUser = document.getElementById('user');
+  const newSpanElement = document.createElement('span');
+  newSpanElement.id = 'user-span';
+  newSpanElement.setAttribute('data-testid', 'online-user');
+  newSpanElement.textContent = nickname;
+  spanUser.appendChild(newSpanElement);
+};
+
 nickButton.addEventListener('click', () => {
   nickname = nickInput.value;
+  addOnlineUser(nickname);
   nickInput.value = '';
 });
 
 const addMessage = (msg) => {
   const li = document.createElement('li');
   li.textContent = msg;
+  li.setAttribute('data-testid', 'message');
   messages.appendChild(li);
   window.scrollTo(0, document.body.scrollHeight);
 };
@@ -29,3 +45,4 @@ form.addEventListener('submit', (e) => {
 });
 
 socket.on('message', addMessage);
+socket.on('connected', addOnlineUser);
--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -4,6 +4,8 @@ const messageMoment = moment().format('DD-MM-yyyy HH:mm:ss A');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
+    io.emit('connected', socket.id.substr(0, 16));
+
     socket.on('message', ({ chatMessage, nickname }) => {
       io.emit('message', `${messageMoment} - ${nickname}: ${chatMessage}`);
     });
