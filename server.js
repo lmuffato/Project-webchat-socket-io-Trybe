74b59f0cf99dbe42e4d7c2b4bd7c18ae4fd0f873
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-10-27 14:55:37 -0400

    add: socket.io front and back

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
 
-// const webchatController = require('./controllers/webchat');
+io.on('connection', (socket) => {
+  console.log(`ID: ${socket.id} entrou!`);
+});
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -18,6 +25,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/webchat', (_req, res) => {
   res.render('chat');
+});
+
+app.post('/webchat', (req, res) => {
+  const { newMessage } = req.body;
+  console.log(newMessage)
+  io.emit('webchat', newMessage);
+  res.status(201).json({ message: 'Success' });
 });
 
 http.listen(PORT, () => console.log(`API escutando na porta ${PORT}`));
