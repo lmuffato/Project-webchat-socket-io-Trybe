1dd9b8732b7be49fbd54c0eaff7f0ce70d3cf713
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-10-27 19:33:41 -0400

    add: change server.js file and webchat.js on controllers

--- controllers/webchat.js
+++ controllers/webchat.js
@@ -1,28 +1,5 @@
-const webchatModel = require('../models/webchat');
-
-const socket = window.io();
-
-const saveNicknameButton = document.querySelector('#nickname-button');
-saveNicknameButton.addEventListener('click', () => {
-  socket.emit('ping');
-  return false;
+module.exports = (io) => io.on('connection', async (socket) => {
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    io.emit('message', `${nickname}: ${chatMessage}`);
+  });
 });
-
-const createMessage = (message) => {
-  const messagesUl = document.querySelector('#messages');
-  const li = document.createElement('li');
-  li.innerText = message;
-  messagesUl.appendChild(li);
-};
-
-const getAll = async (_req, res) => {
-  const result = await webchatModel.getAll();
-  res.status(200).render('../views/chat.ejs', { result });
-};
-
-socket.on('pong', (mensagem) => createMessage(mensagem));
-
-module.exports = {
-  getAll,
-  createMessage,
-};
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 const http = require('http').createServer(app);
+
 const io = require('socket.io')(http, {
   cors: {
     origin: `http://localhost:${PORT}`,
@@ -13,21 +15,15 @@ const io = require('socket.io')(http, {
   },
 });
 
-io.on('connection', (socket) => {
-  socket.on('webchat', (newMessage) => {
-    io.emit('webchat', newMessage);
-  });
-  console.log(`ID: ${socket.id} entrou!`);
-});
+app.use(cors());
+
+require('./controllers/webchat')(io);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get('/', (_req, res) => {
-  res.render('chat');
+  res.status(201).render('chat');
 });
 
 http.listen(PORT, () => console.log(`API escutando na porta ${PORT}`));
