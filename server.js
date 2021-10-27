534e3bbf872f64af4e66ddac36c7fcaabe5cc973
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-27 14:09:52 -0300

    Refactoring server.js to try making evaluator work

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
+const express = require('express');
 const http = require('http');
 const dotenv = require('dotenv');
-const socketIO = require('socket.io');
-const app = require('./app');
 
 dotenv.config();
+const app = express();
+
 const server = http.createServer(app);
-const io = socketIO(server, {
+const io = require('socket.io')(server, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
 });
-const message = require('./sockets/message');
 
-message(io);
+require('./sockets/message')(io);
 
-const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/test', (_req, res) => {
+  res.status(200).render('test-client');
+});
+
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
