5520bc5adcae4d70fdd48eac75d888783864e46c
Joao Nasc <oi@joaonasc.dev>
2021-10-27 20:59:50 -0300

    feat: set public dir to serve static files

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
   res.render('./index');
