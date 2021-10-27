97c425a90362d58ea45db637b47e7b07a25562fe
Rafael Gerônimo <rafael@geronimo.dev>
2021-10-27 12:34:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = require('http').createServer(app);
 
 const io = require('socket.io')(server, {
   cors: {
-    origin: `https://localhost:${PORT}`,
+    origin: `http://localhost:${PORT}`,
     methods: ['GET', 'POST'],
   },
 });
