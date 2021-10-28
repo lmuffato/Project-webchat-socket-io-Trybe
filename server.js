c1d7c6678f7687441df35eb6ab986e08aeb72fe0
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-28 09:21:42 -0300

    fix: bugs

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const serverHttp = http.createServer(app);
 const options = {
   cors: {
     origin: process.env.BACKEND_URL,
+    // origin: '*',
     methods: ['GET', 'POST'],
   },
 };
@@ -27,6 +28,7 @@ const options = {
 const io = new Server(serverHttp, options);
 const users = {};
 
+// eslint-disable-next-line max-lines-per-function
 io.on('connection', (socket) => {
   console.log(`usuário ${socket.id} conectado!`);
   users[socket.id] = socket.id.slice(0, 16);
@@ -34,21 +36,22 @@ io.on('connection', (socket) => {
   socket.on('nickname', (nickname) => {
     users[socket.id] = nickname;
     io.emit('nickname', Object.values(users));
-    // console.log(users);
+    // socket.broadcast.emit('nickname', Object.values(users).reverse());
   });
 
   socket.on('message', async ({ nickname, chatMessage }) => {
     const timestamp = moment().format('DD-MM-YYYY HH:mm:ss A');
-    // const nickname = users[data.nickname];
-    const message = `${timestamp} - ${nickname}: ${chatMessage}`;
+    const nickName = users[nickname];
+    const message = `${timestamp} - ${nickName}: ${chatMessage}`;
 
-    await webChatModel.createMessage({ chatMessage, nickname });
+    await webChatModel.createMessage({ chatMessage, nickname: nickName });
     io.emit('message', message);
   });
-  
+
   socket.on('disconnect', () => {
     console.log(`usuário ${socket.id} desconectado!`);
     delete users[socket.id];
+    io.emit('nickname', Object.values(users));
   });
 
   io.emit('nickname', Object.values(users));
