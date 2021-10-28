13fe58971c4b7a9fed43362ba7ab15fecf807b49
Felipe Flores <felipefloresvendas@gmail.com>
2021-10-28 08:26:32 -0300

    fix update nick user reverse order

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,6 +1,6 @@
 const sendMessage = require('./sendMessage');
 
-const usersOn = {};
+let usersOn = {};
 
 // eslint-disable-next-line max-lines-per-function
 module.exports = (io) => io.on('connection', (socket) => {
@@ -8,6 +8,23 @@ module.exports = (io) => io.on('connection', (socket) => {
   const randomName = socket.id.slice(-16);
   
   usersOn[socket.id] = randomName;
+
+  const users = Object.entries(usersOn);
+
+  if (users[0][0] !== socket.id) {
+    usersOn = {};
+    const arrLength = users.length;
+    const lastUser = users[arrLength - 1];
+    // const firstUser = users[0];
+    users.splice(0, 0, lastUser);
+    users.splice(users.length - 1, 1);
+    // const newArr = users.replace(firstUser, lastUser);
+    for (let i = 0; i < arrLength; i += 1) {
+      const key = users[i][0];
+      const value = users[i][1];
+      usersOn[key] = value;
+    }
+  }
 
   io.emit('nickname', randomName);
   io.emit('login', usersOn);
