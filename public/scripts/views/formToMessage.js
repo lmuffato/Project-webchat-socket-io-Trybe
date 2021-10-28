be6378d42203e31044229d166cb0bfa2f6ed1016
Joao Nasc <oi@joaonasc.dev>
2021-10-27 22:08:10 -0300

    feat: create random nickname on load

--- public/scripts/views/formToMessage.js
+++ public/scripts/views/formToMessage.js
@@ -1,8 +1,48 @@
+const names = [
+  'addison',
+  'brooke',
+  'carter',
+  'dane',
+  'easton',
+  'glen',
+  'harley',
+  'jack',
+  'kai',
+  'lee',
+  'marley',
+  'owen',
+  'piper',
+  'remi',
+  'shae',
+  'tate',
+  'winter',
+];
+
+function randomNickname() {
+  const randomIndex = Math.round(Math.random() * names.length);
+  const randomNumber = Math.round(Math.random() * 9999999999999999);
+  return names[randomIndex].padEnd(16, `-${randomNumber}`);
+}
+
+const form = {
+  nameInput: document.forms[0][0],
+  messageInput: document.forms[0][1],
+};
+
+const nickname = randomNickname();
+document.getElementById('user').textContent = nickname;
+form.nameInput.value = nickname;
+
+form.nameInput.onkeyup = ({ target }) => {
+  document.getElementById('user').textContent = target.value;
+  if (target.value === '') document.getElementById('user').textContent = nickname;
+};
+
 function newMessage(event) {
   event.preventDefault();
   const { socket } = window;
-  const nameInput = document.forms[0][0];
-  const messageInput = document.forms[0][1];
+  const { nameInput, messageInput } = form;
+  if (nameInput.value === '') nameInput.value = nickname;
   socket.emit('message', { 
     nickname: nameInput.value, chatMessage: messageInput.value,
   });
