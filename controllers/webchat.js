b5703d47bed44f3c6ecdb4c69816b6a57cdcc4ee
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-10-27 20:14:58 -0400

    feat: Req 1 - Crie um back-end para conexão simultânea de clientes e troca de mensagens em chat público

--- controllers/webchat.js
+++ controllers/webchat.js
@@ -1,5 +1,14 @@
+const time = () => {
+  const now = new Date();
+  const date = `${now.getDate()}-${now.getMonth()}-${now.getFullYear()}`;
+  const hour = `${now.getHours()}-${now.getMinutes()}`;
+  const msgTime = `${date} ${hour}`;
+  return msgTime;
+};
+
 module.exports = (io) => io.on('connection', async (socket) => {
+  time();
   socket.on('message', async ({ msg, nick }) => {
-    io.emit('message', `${nick}: ${msg}`);
+    io.emit('message', `${time()} - ${nick}: ${msg}`);
   });
 });
