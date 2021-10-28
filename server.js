ff3b7661e88033e79ca1def7f9cb37bc2731dd4a
Lucas Lara <lucasml@live.com>
2021-10-28 20:56:50 -0300

    Server Log return listening

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const app = express();
 
 const server = require('http').createServer(app);
 
+const { Server } = require('socket.io');
+
+const io = new Server(server);
+
 const PORT = 3000;
 
 app.use(express.static('./public'));
 
 app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, 'public/chat.html'));
+});
+
+io.on('connection', () => {
+  console.log('Usuário conectado');
 });
 
 server.listen(PORT, () => console.log(`Server WebChat Online na porta ${PORT}`)); 
