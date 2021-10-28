ff9444d3d13954b5483a1ef27924eaf2767a666e
lucas-paz <lucas.hpaz@outlook.com>
2021-10-28 10:29:43 -0300

    feat req4

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -4,15 +4,13 @@ const Chat = require('../models/chatModels');
 const date = moment().format('DD-MM-YYYY HH:mm:ss A');
 const users = {};
 
-// eslint-disable-next-line max-lines-per-function
 const handleSocket = (io) => {
   io.on('connection', (socket) => {
-    console.log('xablau');
     // socket.disconnect(0);
     const defaultName = socket.id.substring(0, 16);
     users[socket.id] = defaultName;
-    socket.emit('defaultName', { defaultName });
-    io.emit('users', { users, id: socket.id });
+    socket.emit('defaultName', { defaultName, id: socket.id });
+    io.emit('users', users);
 
     socket.on('message', ({ chatMessage, nickname }) => {
       io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
@@ -21,12 +19,12 @@ const handleSocket = (io) => {
 
     socket.on('nameChange', (name) => {
       users[socket.id] = name;
-      io.emit('users', { users, id: socket.id });
+      io.emit('users', users);
     });
 
     socket.on('disconnect', () => {
       delete users[socket.id];
-      io.emit('users', { users, id: socket.id });
+      io.emit('users', users);
     });
   });
 };
