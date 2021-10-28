d72192f72af95b69107816f59f92c43b1730b8bd
rafael <rafittu@outlook.com>
2021-10-28 16:01:23 -0300

    feat: req 2 - implementa frontend para que as pessoas interajam com o chat

--- assets/javascripts/client.js
+++ assets/javascripts/client.js
@@ -1,18 +1,45 @@
 const socket = window.io('http://localhost:3000');
 
-// AQUI ENVIA PRO EJS MONTAR A PAGINA
+// aqui está alimentando o csr
+
 socket.on('message', (data) => {
   const li = document.createElement('li');
   const liText = document.createTextNode(data);
+  li.setAttribute('data-testid', 'message');
   li.append(liText);
   document.getElementById('board-message').appendChild(li);
 });
 
-// O NICKNAME PRECISA CHEGAR AQUI E SER RETORNADO NA LI-16 JUNTO COM A MESSAGE
+const setNickname = (nickname) => {
+  const li = document.createElement('li');
+  const liNick = document.createTextNode(nickname);
+  li.setAttribute('data-testid', 'online-user');
+  li.append(liNick);
+  document.getElementById('online-users').appendChild(li);
+};
+
+// da para organizar em outra pasta daqui pra baixo (?)
+let nickname;
+
+socket.on('nicknameGenerator', ({ newNickname }) => setNickname(newNickname));
+socket.on('nickname', (userId) => {
+  nickname = userId;
+  setNickname(nickname);
+});
+
+const nicknameBtn = document.getElementById('nickname-button');
+nicknameBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const nicknameInput = document.getElementById('input-nickname');
+  nickname = nicknameInput.value;
+  socket.emit('nickname', nickname);
+  nicknameInput.value = '';
+});
+
 const sendBtn = document.getElementById('send-button');
 sendBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const text = document.getElementById('input-message');
-  socket.emit('message', { chatMessage: text.value });
+  socket.emit('message', { nickname, chatMessage: text.value });
   text.value = '';
 });
--- sockets/webchatSocket.js
+++ sockets/webchatSocket.js
@@ -2,7 +2,12 @@ const moment = require('moment');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    // const nickname = socket.id.substring(0, 16);
+    const userId = socket.id.substring(0, 16);
+    io.emit('nickname', userId);
+  
+    socket.on('nickname', (nick) => {
+      io.emit('nicknameGenerator', { nickname: nick });
+    });
   
     socket.on('message', ({ nickname, chatMessage }) => {
       const timestamp = moment().format('DD-MM-yyyy hh:mm:ss A');
