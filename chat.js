9b27346d7961aed619b9cfb0d5f5532e77f3789a
biazidioti <bzferreira.bio@gmail.com>
2021-10-28 11:41:39 -0300

    Express app listening on port 5000

---
+++ chat.js
@@ -0,0 +1,18 @@
+// Front connection
+
+const socket = window.io('http://localhost:3000');
+
+const createLi = (notification) => {
+  const li = document.createElement('li');
+  const liText = document.createTextNode(notification);
+  li.appendChild(liText);
+  document.getElementById('news').appendChild(li);
+};
+
+socket.on('notification', createLi);
+
+socket.on('loadNotifications', (notifications) => {
+  notifications.forEach(notification => {
+    createLi(notification);
+  });
+});
--- server.js
+++ server.js
@@ -1,1 +1,59 @@
-// Faça seu código aqui
+// Back-end server
+const express = require('express'); // ok
+const bodyParser = require('body-parser'); // ok
+
+require('dotenv').config();
+
+const app = express(); // ok
+
+/* const socketIoServer = require('http').createServer(app);
+const io = require('socket.io')(socketIoServer, {
+  cors: { // Aqui existe um objeto de configuração, essas options são necessárias a partir da major 3 do socket.io 
+    origin: `http://localhost:${EXPRESS_PORT}`, // origem permitida
+    methods: ['GET', 'POST'], // métodos permitidos
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.emit('loadNotifications', NEWS);
+  console.log(`novo usuário ${socket.id}  conectado ao socket.io`);
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views'); */
+
+app.use(bodyParser.json()); // ok
+app.use(bodyParser.urlencoded({ extended: true })); // ok
+
+/* app.use('/assets', express.static('./assets/javascripts'));
+app.use('/assets', express.static('./assets/css'));
+ */
+/* const NEWS = []; */
+
+/* app.get('/board/ssr', (req, res) => {
+  res.render('board/ssr', { notifications: NEWS });
+});
+
+app.get('/board/csr', (req, res) => {
+  res.render('board/csr');
+}); */
+
+app.post('/message', (req, res) => { // ok
+  const { notification } = req.body;
+
+/*   NEWS.push(notification);
+
+  io.emit('message', notification); */
+  console.log(notification);
+
+  res.status(201).json({ message: 'Notified successfully' });
+});
+
+app.listen(
+  process.env.EXPRESS_PORT, () => 
+  console.log(`Express app listening on port ${process.env.SOCKETIO_PORT}!`),
+  );
+
+/* socketIoServer.listen(
+  process.env.EXPRESS_PORT, 
+  console.log(`Socket.io server listening on port ${process.env.SOCKETIO_PORT}!`)); // ok */
