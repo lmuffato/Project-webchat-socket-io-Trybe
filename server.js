49b5c6303f693ef1909e497d9ac014ad7e931dc1
luanlsr <luanfswd@gmail.com>
2021-10-28 15:49:56 -0300

    feat: :sparkles: req1 implemented

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
 const express = require('express');
 const path = require('path');
+const moment = require('moment');
+// const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const { PORT } = process.env;
 
-app.use(express.static(path.join(__dirname, '/views/public')));
-app.set('views', path.join(__dirname, '/views/public'));
+// app.use(cors());
+const http = require('http').createServer(app);
 
-app.engine('html', require('ejs').renderFile);
+const corsOptions = {
+  origin: `http://localhost:${PORT}`,
+  methods: ['GET', 'POST'],
+};
+
+const io = require('socket.io')(http, corsOptions);
 
 app.set('view engine', 'html');
 
-app.use('/', (_req, res) => {
-  res.render('index.html');
-});
-
-const messages = [];
+app.use(express.static(path.join(__dirname, 'views')));
+app.set('views', path.join(__dirname, 'views'));
 
 io.on('connection', (socket) => {
-  console.log(`Socket conectado: ${socket.id}`);
+  console.log(`Usuário conectado: ${socket.id}`);
 
-  socket.emit('previuosMessages', messages);
-
-  socket.on('sendMessage', (data) => {
-    messages.push(data);
-    console.log(data);
-    socket.broadcast.emit('recivedMessage', data);
+  socket.on('message', ({ nickname, chatMessage }) => {
+    const timeStamp = moment().format('DD-MM-YYYY HH:mm');
+    io.emit('message', `${timeStamp} - ${nickname}: ${chatMessage}`);
   });
 });
 
-server.listen(3000, () => {
-  console.log('Servidor online!');
-  console.log('http://localhost:3000');
+app.use('/', (_req, res) => {
+  res.render('index.ejs');
 });
+
+http.listen(PORT, () => console.log(`Socket online na ${PORT}
+Acessar: http://localhost:${PORT}`));
