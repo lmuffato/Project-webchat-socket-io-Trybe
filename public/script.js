0f8b5c5deeefbbc609e46b6343fbf7a682c08a5e
iago <iago.pferreiravr@gmail.com>
2021-10-28 10:45:13 -0300

    Feat: Req2

--- public/script.js
+++ public/script.js
@@ -1,10 +1,22 @@
 const socket = window.io();
-const button = document.querySelector('#pingButton');
-console.log(button);
+const sendBtn = document.querySelector('#send-button');
+const nicknameBtn = document.querySelector('#nickname-button');
+let nick;
 
-button.addEventListener('click', () => {
+socket.on('connect', () => {
+  nick = socket.id;
+});
+
+sendBtn.addEventListener('click', () => {
   const input = document.querySelector('#message-input');
-  socket.emit('message', { chatMessage: input.value, nickname: socket.id });
+  socket.emit('message', { chatMessage: input.value, nickname: nick });
+  input.value = '';
+  return false;
+});
+
+nicknameBtn.addEventListener('click', () => {
+  const input = document.querySelector('#nickname-input');
+  socket.emit('nickname', { nickname: input.value, id: socket.id });
   input.value = '';
   return false;
 });
@@ -13,7 +25,23 @@ const createMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
   li.innerText = message;
+  li.setAttribute('data-testid', 'message');
   messagesUl.appendChild(li);
 };
 
 socket.on('message', (message) => createMessage(message));
+
+socket.on('nickname', ({ id, nickname }) => {
+  const updateNickname = document.querySelector(`#${id}`);
+  updateNickname.innerText = nickname;
+  nick = nickname;
+});
+
+socket.on('user', (id) => {
+  const users = document.querySelector('#online-users');
+  const li = document.createElement('li');
+  li.innerText = id;
+  li.setAttribute('data-testid', 'online-user');
+  li.setAttribute('id', id);
+  users.appendChild(li);
+});
