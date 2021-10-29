1d08be2073a1a231f791c673de84a0832f6a3641
AndreBarroso <andre_barroso17@hotmail.com>
2021-10-29 13:43:31 -0300

    feat: cria script para alterar o nome do user

--- views/js/chat.js
+++ views/js/chat.js
@@ -1,21 +1,33 @@
 const socket = window.io();
 
+const changeCurrentNickname = document.getElementById('changeCurrentNickname');
+const usersLogged = document.getElementById('usersLogged');
+let nickWithNumbers;
+
 const insertNicknameUserLogged = (nickname) => {
-  const usersLogged = document.getElementById('usersLogged');
   const newUserLogged = document.createElement('li');
   newUserLogged.innerText = nickname;
   newUserLogged.setAttribute('data-testid', 'online-user');
+  newUserLogged.id = nickname;
   usersLogged.appendChild(newUserLogged);
 };
 
 socket.emit('initConnection');
 
 socket.on('showNicknamesOfUsersLoggeds', (newNicknameFormated) => {
+  nickWithNumbers = newNicknameFormated;
   insertNicknameUserLogged(newNicknameFormated);
 
   // sessionStorage.setItem('nickname', JSON.stringify(newNicknameFormated));
 
   // sessionStorage.setItem('nickname', JSON.stringify(newNicknameFormated));
+});
+
+changeCurrentNickname.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nicknameBox = document.getElementById('nickname-box');
+  const currentUser = document.getElementById(nickWithNumbers);
+  currentUser.innerText = nicknameBox.value;
 });
 
 // const { username, room } = Qs.parse(location.search, {
