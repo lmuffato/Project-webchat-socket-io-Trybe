e44cc2478ccc2560ceb7a02f6c003c59738feb9e
AndreBarroso <andre_barroso17@hotmail.com>
2021-10-29 11:31:10 -0300

    feat: rendenriza no front, o nick do user

--- sockets/chat.js
+++ sockets/chat.js
@@ -2,8 +2,17 @@ const moment = require('moment');
 
 const date = moment().format('DD-MM-YYYY HH:mm:ss A'); // https://momentjs.com/
 module.exports = (io) => io.on('connection', async (socket) => {
-    console.log(`Usuárioooooooooo conectado. ID: ${socket.id} `);
-  
+    const newNicknName = socket.id.slice(0, 16);
+
+    console.log('testa', newNicknName);
+
+    socket.on('initConnection', () => console.log('Novo usuário logado'));
+
+    socket.on('initConnection', () => {
+      socket.emit('showNicknamesOfUsersLoggeds', `${newNicknName}`); 
+      console.log('Novo usuário logado');
+    });
+
     socket.on('message', ({ chatMessage, nickname }) => {
       io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
     });
--- views/js/chat.js
+++ views/js/chat.js
@@ -1,5 +1,23 @@
 const socket = window.io();
 
+const insertNicknameUserLogged = (nickname) => {
+  const usersLogged = document.getElementById('usersLogged');
+  const newUserLogged = document.createElement('li');
+  newUserLogged.innerText = nickname;
+  newUserLogged.setAttribute('data-testid', 'online-user');
+  usersLogged.appendChild(newUserLogged);
+};
+
+socket.emit('initConnection');
+
+socket.on('showNicknamesOfUsersLoggeds', (newNicknameFormated) => {
+  insertNicknameUserLogged(newNicknameFormated);
+
+  // sessionStorage.setItem('nickname', JSON.stringify(newNicknameFormated));
+
+  // sessionStorage.setItem('nickname', JSON.stringify(newNicknameFormated));
+});
+
 // const { username, room } = Qs.parse(location.search, {
 //   ignoreQueryPrefix: true});
 
