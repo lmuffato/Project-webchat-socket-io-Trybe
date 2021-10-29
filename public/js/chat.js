27f6a0a7d556f1f106ccda85d573a5aea686d5bb
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-29 15:08:45 -0300

    Implemented a fuction where create a message to render in the screen

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,3 +12,14 @@ form.addEventListener('submit', (e) => {
 
   return false;
 });
+
+const createMessage = (message) => {
+  const messageUl = document.querySelector('#messages');
+  const li = document.createElement('li');
+
+  li.innerText = message;
+
+  messageUl.appendChild(li);
+};
+
+socket.on('serverMessage', (message) => createMessage(message));
