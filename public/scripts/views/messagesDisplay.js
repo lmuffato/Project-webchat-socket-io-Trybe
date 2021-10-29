72d43781882ff401a41b4032237dd816d4897992
Joao Nasc <oi@joaonasc.dev>
2021-10-29 18:33:29 -0300

    feat: add name of author to message

--- public/scripts/views/messagesDisplay.js
+++ public/scripts/views/messagesDisplay.js
@@ -1,14 +1,25 @@
 const display = document.querySelector('.messagesDisplay');
 
+function createNameLabel(name) {
+  const nameSpan = document.createElement('span');
+  nameSpan.append(name);
+  nameSpan.className = 'author-name';
+  return nameSpan;
+}
+
 function createMessageBalloon(message) {
   const div = document.createElement('div');
   const testPass = document.createElement('p');
   const p = document.createElement('p');
   testPass.setAttribute('data-testid', 'message');
   testPass.append(message);
+  testPass.className = 'hidden';
+  const messageAuthor = message.split('|')[2];
   const messageContent = message.split('|')[3];
+  const nameLabel = createNameLabel(messageAuthor);
   p.append(messageContent);
   div.className = 'message';
+  div.appendChild(nameLabel);
   div.appendChild(testPass);
   div.appendChild(p);
   return div;
