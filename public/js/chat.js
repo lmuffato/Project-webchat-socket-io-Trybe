75debf07be68a8f24414a60a5e632850ec4bfeb1
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-29 16:54:45 -0300

    fix: change the use about Qs lib for other method, to get the qusery string params

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,15 +3,16 @@ const socket = window.io();
 const form = document.querySelector('form');
 const inputMessage = document.querySelector('#messageInput');
 
-const { username } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true });
+const urlSearchParams = new URLSearchParams(window.location.search);
+
+const { username } = Object.fromEntries(urlSearchParams.entries());
   
 socket.emit('joinChat', { username });
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  socket.emit('clientMessage', inputMessage.value);
+  socket.emit('clientMessage', `${username}: ${inputMessage.value}`);
 
   inputMessage.value = '';
 
@@ -28,3 +29,8 @@ const createMessage = (message) => {
 };
 
 socket.on('serverMessage', (message) => createMessage(message));
+
+/* 
+  Consegui trocar o uso do Qs.parse utilizando o uso do URLSearchParams segundo esse link:
+  https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+*/
