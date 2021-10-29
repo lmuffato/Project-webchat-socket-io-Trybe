52ca864d328da47a8eb5af2b80339de773eef229
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-29 14:58:45 -0300

    Created the initial config for the server file

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 // Faça seu código aqui
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+require('./sockets/chat')(io);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
