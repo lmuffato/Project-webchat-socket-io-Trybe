c49534f437f28f90a44245a0ee652dd3382e7885
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-10-29 18:14:33 -0300

    feat: sockets logic implemented

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+const moment = require('moment');
+// https://momentjs.com/
+
+const app = express();
+const port = 3000;
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.status(200).render('chat');
+});
+
+const timeNow = moment().format('DD-MM-YYYY hh:mm:ss');
+io.on('connection', (socket) => {
+  console.log('ihhh foi conectado');
+  socket.on('message', ({ chatMessage, nickname }) => {
+    io.emit('message', `${timeNow} - ${nickname} - ${chatMessage}`);
+  });
+});
+
+app.use(express.static(path.join(__dirname, 'chat')));
+
+http.listen(port, () => console.log(`Example app listening on ${port}!`));
