7923a2539c1b73ee65d828fc81ab46a7585e74d1
maricegalla <maricegalla@gmail.com>
2021-10-29 16:11:34 -0300

    feat: :sparkles: Requisito 2

--- public/script/client.js
+++ public/script/client.js
@@ -2,14 +2,39 @@ const socket = window.io();
 
 const sendButton = document.querySelector('#sendButton');
 const inputMessage = document.querySelector('#messageBox');
+const saveButton = document.querySelector('#nicknameButton');
+const inputNickname = document.querySelector('#nicknameBox');
 
 const DATA_TESTID = 'data-testid';
+const ID = 'id';
+const TITLE = 'title';
 
 sendButton.addEventListener('click', (e) => {
   e.preventDefault();
-  socket.emit('message', { chatMessage: inputMessage.value, nickname: socket.id.slice(1, 17) });
+  const nick = document.getElementById(socket.id.slice(0, 16)).title;
+  socket.emit('message', { chatMessage: inputMessage.value, nickname: nick });
   inputMessage.value = '';
 });
+
+saveButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (inputNickname.value) {
+    const newNickname = inputNickname.value;
+    const greeting = document.querySelector('#greeting').firstChild;
+    greeting.innerText = `Olá, ${newNickname}!`;
+    greeting.setAttribute(TITLE, newNickname);
+    inputNickname.value = '';
+    socket.emit('newNickname', newNickname);
+  }
+});
+
+const userGreeting = (userName) => {
+  const userSpan = document.querySelector('#greeting');
+  const p = document.createElement('p');
+  p.setAttribute(ID, userName);
+  p.innerText = `Olá, ${userName}!`;
+  userSpan.appendChild(p);
+};
 
 const newMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
@@ -20,3 +45,24 @@ const newMessage = (message) => {
 };
 
 socket.on('message', (chatMessage) => newMessage(chatMessage));
+
+const onlineUsers = (users) => {
+  const userUl = document.querySelector('#userList');
+  const li = document.createElement('li');
+  li.setAttribute(DATA_TESTID, 'online-user');
+  li.setAttribute(ID, users);
+  li.innerText = users;
+  userUl.appendChild(li);
+};
+
+socket.on('userList', (users) => {
+  const userUl = document.querySelector('#userList');
+  const usersQty = userUl.childElementCount;
+  if (usersQty > 0) {
+    while (userUl.firstChild) {
+      userUl.removeChild(userUl.lastChild);
+    }
+  }
+  users.forEach((user) => onlineUsers(user)); 
+});
+socket.on('userName', (userName) => userGreeting(userName));
--- sockets/chat.js
+++ sockets/chat.js
@@ -2,10 +2,25 @@ const moment = require('moment');
 
 const timestamp = moment().format('DD-MM-YYYY HH:mm:ss');
 
+const users = [];
+
 module.exports = (io) => {
-  io.on('connection', (socket) => { 
+  io.on('connection', async (socket) => {
+    users[socket.id.slice(0, 16)] = socket.id.slice(0, 16);
+    const usersNick = Object.values(users);
+    const nick = usersNick.filter((n) => n === socket.id.slice(0, 16));
+    socket.emit('userName', nick);
+    io.emit('userList', Object.values(users));
     socket.on('message', ({ chatMessage, nickname }) => {
       io.emit('message', `${timestamp} - ${nickname} : ${chatMessage}`);
+    });
+    
+    socket.on('newNickname', (nickname) => {
+      console.log(nickname);
+      users[socket.id.slice(0, 16)] = nickname;
+      console.log(usersNick, users);
+
+      io.emit('userList', Object.values(users));
     });
   });
 };
