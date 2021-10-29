2b597ba94c72075026151016ec8e8b724154193e
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-29 15:01:51 -0300

    fix: the server endpoint and create a html base file for chat

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 require('./sockets/chat')(io);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/chat.html'));
 });
 
 http.listen(3000, () => {
