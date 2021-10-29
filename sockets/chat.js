1f5a5d95560137c2edd07c3f3eaab5fdedbc689b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-29 15:36:14 -0300

    Inserted a message to the chat with the user whats connected

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,3 +1,5 @@
 module.exports = (io) => io.on('connection', (socket) => {
   socket.emit('serverMessage', 'Bem-vindo ao chat público!');
+
+  socket.broadcast.emit('serverMessage', `${socket.id} acabou de conectar`);
 });
