2df8e664c9978b26d537bfbffb7104e07de60f1a
giovanni-maldonado <giovannitadeu08@gmail.com>
2021-10-29 19:11:26 -0300

    feat: requisito 2

---
+++ public/webChat.js
@@ -0,0 +1,62 @@
+const socket = window.io();
+
+const nickInput = document.getElementById('nickInput');
+const nickBtn = document.getElementById('nickBtn');
+const messageInput = document.getElementById('messageInput');
+const sendMessageBtn = document.getElementById('sendMessageBtn');
+const listMessages = document.getElementById('messagesField');
+const listUsers = document.getElementById('userList');
+
+const saveNickname = () => {
+  if (nickInput.value) {
+    sessionStorage.setItem('nickname', nickInput.value);
+  }
+};
+
+const getNickname = () => {
+  const nickname = sessionStorage.getItem('nickname');
+  return nickname;
+};
+
+const createUser = (id) => {
+  const li = document.createElement('li');
+  li.setAttribute('data-testid', 'online-user');
+  li.innerText = id;
+  listUsers.appendChild(li);
+};
+
+const createMessage = (message) => {
+  const li = document.createElement('li');
+  li.setAttribute('data-testid', 'message');
+  li.innerText = message;
+  listMessages.appendChild(li);
+};
+
+nickBtn.addEventListener('click', () => {
+  saveNickname();
+  if (nickInput.value) {
+    socket.emit('nickname', nickInput.value);
+    nickInput.value = '';
+  }
+});
+
+sendMessageBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const nickname = getNickname() || socket.id.substring(0, 16);
+  if (messageInput.value) {
+    socket.emit('message', { chatMessage: messageInput.value, nickname });
+  }
+  messageInput.value = '';
+});
+
+socket.on('message', (message) => createMessage(message));
+
+socket.on('getMessages', (messages) => {
+  messages.forEach(({ timeStamp, nickname, chatMessage }) =>
+    createMessage(`${timeStamp} - ${nickname}: ${chatMessage}`));
+});
+
+socket.on('showUsers', (users) => {
+  listUsers.innerHTML = '';
+  users.forEach((user) => createUser(user));
+});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const moment = require('moment');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,16 +13,27 @@ const io = require('socket.io')(http, {
   },
 });
 
+const Users = [];
+
 io.on('connection', (socket) => {
   socket.on('message', ({ chatMessage, nickname }) => {
     const timeStamp = moment().format('DD-MM-YYYY HH:mm:ss A');
     io.emit('message', `${timeStamp} - ${nickname}: ${chatMessage}`);
+    console.log(`${timeStamp} - ${nickname}: ${chatMessage}`);
+  });
+  Users.push(socket.id.substring(0, 16));
+  io.emit('showUsers', Users);
+
+  socket.on('disconnect', () => {
+    Users.pop();
+    io.emit('show_Users', Users);
   });
 });
 
 app.use(cors());
+app.use(express.static(`${__dirname}/public`));
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', path.join(__dirname, './views'));
 app.use('/', (req, res) => {
   res.render('index.ejs');
 });
