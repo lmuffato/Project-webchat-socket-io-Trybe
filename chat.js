5c65ac55155826ab4304645afc6e2f7fb6c15346
Hudson Carlos <hc88806@gmail.com>
2021-10-29 18:13:48 -0400

    front-end done

--- chat.js
+++ chat.js
@@ -2,6 +2,8 @@ const moment = require('moment');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
+    socket.emit('idNickname', socket.id.substr(4, 20));
+
     socket.on('message', ({ chatMessage, nickname }) => {
       const dateAndOur = moment().format('DD-MM-YYYY h:mm:ss A');
       io.emit('message', `${dateAndOur} - ${nickname}: ${chatMessage}`);
