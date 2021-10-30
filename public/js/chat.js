0ace59e5d69f4c9a1ef4eb43a9837247e97e6773
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-10-30 16:32:14 -0300

    Feat: req 2

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,22 +1,57 @@
 const socket = window.io();
 
 const form = document.querySelector('form');
+const btnNick = document.querySelector('#nickname-button');
 const inputMessage = document.querySelector('.messageInput');
+const inputNick = document.querySelector('#nickname-input');
+const DATATESTID = 'data-testid';
+let personalNick;
+
+// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i += 1) {
+  result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+  }
+  
+const nickname = makeid(16); 
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const nickname = 'xablau';
   const chatMessage = inputMessage.value;
-  socket.emit('message', { chatMessage, nickname });
+  socket.emit('message', { chatMessage, nickname: personalNick || nickname });
   inputMessage.value = '';
+  return false;
+});
+
+btnNick.addEventListener('click', (_e) => {
+  personalNick = inputNick.value;
+  document.getElementById(nickname).innerText = inputNick.value;
+  inputNick.value = '';
   return false;
 });
 
 const createMessage = (message) => {
   const messagesUl = document.querySelector('.messages');
   const li = document.createElement('li');
+  li.setAttribute(DATATESTID, 'message');
   li.innerText = message;
   messagesUl.appendChild(li);
 };
 
-socket.on('message', (message) => createMessage(message));
+const onlineUsers = () => {
+  const usersUl = document.querySelector('#online-users');
+  const li = document.createElement('li');
+  li.setAttribute(DATATESTID, 'online-user');
+  li.setAttribute('id', nickname);
+  li.innerText = nickname;
+  usersUl.appendChild(li);
+  return false;
+};
+
+  socket.on('message', (message) => createMessage(message));
+  window.onload(onlineUsers());
