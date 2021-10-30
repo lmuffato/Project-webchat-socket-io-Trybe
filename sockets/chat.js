e8854d55a3f5ad077eee2e7f8393c14d644a1228
maricegalla <maricegalla@gmail.com>
2021-10-29 22:52:19 -0300

    fix: :bug: Kill gost sockets

--- sockets/chat.js
+++ sockets/chat.js
@@ -13,23 +13,18 @@ const users = [];
 
 module.exports = (io) => {
   io.on('connection', async (socket) => {
-    console.log(socket.id);
-  
+    socket.disconnect(0);
     users[socket.id.slice(0, 16)] = socket.id.slice(0, 16);
-    
     socket.on('message', async ({ chatMessage, nickname }) => {
       await saveHistory({ timestamp, nickname, chatMessage });
       io.emit('message', `${timestamp} - ${nickname} : ${chatMessage}`);
     });
-    
     socket.emit('showHistory', await messagesHistory());
     io.emit('userList', Object.values(users));
-
     socket.on('newNickname', (nickname) => {
       users[socket.id.slice(0, 16)] = nickname;
       io.emit('userList', Object.values(users));
     });
-
     socket.on('disconnect', () => {
       delete users[socket.id.slice(0, 16)];
       io.emit('userList', Object.values(users));
