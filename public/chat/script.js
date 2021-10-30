f7f126064e73d26137d1002b79ac7e664d9441bd
guilhermemd <guilherme.md@gmail.com>
2021-10-30 16:37:56 -0300

    feat: :sparkles: req 02 realizado com ajuda de Anderson Nascimento turma 10 A

--- public/chat/script.js
+++ public/chat/script.js
@@ -1,13 +1,18 @@
 const socket = window.io();
 
-const msgBtn = document.querySelector('.mensagem__btn');
 const msgInput = document.querySelector('.mensagem__input');
+const msgBtn = document.querySelector('.mensagem__btn');
+
+const nicknameInput = document.querySelector('.nickname__input');
+const nicknameBtn = document.querySelector('.nickname__btn');
+
+// Ideia de Marília turma 10 A
+let nick = '';
 
 msgBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log(socket.id, 'soooooockeeet id');
-  // console.log(socket.id.slice(1, 17), '177777777777');
-  socket.emit('message', { chatMessage: msgInput.value, nickname: socket.id.slice(1, 17) });
+  const nickname = nick || socket.id.slice(0, 16);
+  socket.emit('message', { chatMessage: msgInput.value, nickname });
   msgInput.value = '';
 });
 
@@ -15,7 +20,30 @@ const sentMsg = (message) => {
   const chatMsgUl = document.querySelector('.webchat__mensagens');
   const li = document.createElement('li');
   li.innerText = message;
+  li.setAttribute('data-testid', 'message');
   chatMsgUl.appendChild(li);
 };
 
+nicknameBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  nick = nicknameInput.value;
+  if (nick !== '') {
+    socket.emit('newNickname', nick);
+    nicknameInput.value = '';
+  }
+});
+
+const nicknameList = (users) => {
+  const nicknameUlList = document.querySelector('.nickname__list');
+  nicknameUlList.innerHTML = '';
+  users.forEach((element) => {
+    const li = document.createElement('li');
+    li.setAttribute('data-testid', 'online-user');
+    li.innerText = element;
+    nicknameUlList.appendChild(li);
+  });
+};
+
 socket.on('message', (chatMessage) => sentMsg(chatMessage));
+
+socket.on('usersList', (users) => nicknameList(users));
--- server.js
+++ server.js
@@ -28,11 +28,22 @@ const moment = require('moment');
 
 const timestamp = moment().format('DD-MM-YYYY hh:mm:ss');
 
+const users = {};
 io.on('connection', (socket) => { 
-  console.log(socket.id);
+  // console.log(`O CLiente ${socket.id} está online`);
+  users[socket.id] = socket.id.slice(0, 16);
+  
   socket.on('message', ({ chatMessage, nickname }) => {
     io.emit('message', `${timestamp} - ${nickname} : ${chatMessage}`);
   });
+
+  socket.on('newNickname', (nickname) => {
+    users[socket.id] = nickname;
+    console.log(users);
+    io.emit('usersList', Object.values(users));
+  });
+
+  io.emit('usersList', Object.values(users));
 });
 
 app.get('/', (_req, res) => res.render('chat/index'));
@@ -40,3 +51,5 @@ app.get('/', (_req, res) => res.render('chat/index'));
 http.listen(PORT, () => {
   console.log(`Socket online na ${PORT}, acessar: http://localhost:3000`);
 });
+
+// Requisito 2 foi realizado com ajuda de Anderson Nasimento turma 10 A
