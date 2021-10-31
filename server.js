d85de72736ded8c558cdd5038da11f57fd35e91e
Southier <patrick.southier@hotmail.com>
2021-10-30 23:50:31 -0300

    feat: set the view engine with ejs and refactors the get "/" endpoint using the chatController

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-const express = require('express');
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
 
-const app = express();
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
 const PORT = 3000;
+const chatController = require('./controllers/chatController');
 
-app.listen(PORT, () => console.log(`App runing on port ${PORT}`));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log(`O Cliente ${socket.id} se conectou`);
+});
+
+app.get('/', chatController.getHistory);
+
+http.listen(PORT, () => console.log(`App runing on port ${PORT}`));
