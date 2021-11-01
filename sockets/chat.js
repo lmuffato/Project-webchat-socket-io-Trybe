1034cc93e76a274dad549ae7710f44422d22af04
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-11-01 17:13:06 -0400

    requirement 2.1 random nickname done

--- sockets/chat.js
+++ sockets/chat.js
@@ -8,13 +8,32 @@ obj message = {
 }
  */
 
+const onlineUsers = [];
+
+const findIndexOnlineUser = (socketId) => (
+  onlineUsers.findIndex(({ id }) => socketId.slice(0, 16) === id)
+);
+
 module.exports = (io) => io.on('connection', (socket) => {
   console.log(`Usuário conectado. ID: ${socket.id} `);
+  const nick = socket.id.slice(0, 16);
+  onlineUsers.push({ nickname: nick, id: nick });
+
+  io.emit('online-users', onlineUsers);
 
   socket.on('message', async (data) => {
     const { nickname, chatMessage } = data;
     const timestamp = new Date();
+
     io.emit('message', `${moment(timestamp)
-      .format('DD-MM-yyyy HH:mm:ss a')} - ${nickname}: ${chatMessage}`);
+      .format('DD-MM-yyyy h:mm:ss a')} - ${nickname}: ${chatMessage}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`usuário ${socket.id} desconectado`);
+    const index = findIndexOnlineUser(socket.id);
+    onlineUsers.splice(index, 1);
+
+    io.emit('online-users', onlineUsers);
   });
 });
