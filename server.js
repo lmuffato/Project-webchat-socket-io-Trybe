20e47d2e75617b53914af8ccc35a9e0ee46b6baa
giovanni-maldonado <giovannitadeu08@gmail.com>
2021-11-01 15:27:19 -0300

    feat: backend & requisito 1

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const moment = require('moment');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'https://localhost:3000',
+    method: ['GET, POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const timeStamp = moment().format('DD-MM-YYYY HH:mm:ss A');
+    io.emit('message', `${timeStamp} - ${nickname}: ${chatMessage}`);
+  });
+});
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+http.listen(PORT, () => console.log(`Passando na porta ${PORT}`));
