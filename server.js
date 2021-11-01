bebce158e2d236bb4e9914f5e25b3d67e0c97ebe
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-11-01 10:35:04 -0400

    feat: :sparkles: config iniciais do server

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-// Faça seu código aqui
+require('dotenv').config();
+
+const { Server } = require('socket.io');
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+// ------------------------------------------------------------------------------------------//
+
+const io = new Server(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => { 
+    console.log(`Usuário conectado. ID: ${socket.id} `);
+});
+  
+// ------------------------------------------------------------------------------------------//
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log(`Socket online na ${PORT}, acessar: http://localhost:3000`);
+});
+
+// http: cria o servidor
+// createServer(app): conecta o client e o servidor para trabalharem juntos
+// cors: conexão e métodos
