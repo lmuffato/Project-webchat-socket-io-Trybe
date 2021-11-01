f0a3c5a8d0c0bdf6448fa6548e8bd66a28da0bf2
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-11-01 18:11:28 -0400

    fix: :bug: req 1

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (_req, res) => res.render('chat/webChat'));
+app.get('/', (_req, res) => res.render('chat/index'));
 
 // ------------------------------------------------------------------------------------------//
 
