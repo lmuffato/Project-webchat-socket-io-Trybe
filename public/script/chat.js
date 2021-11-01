272cee3f0f5a50b69bdc249135b59a4290f90abc
Tiago Santos <tiago881@gmail.com>
2021-10-31 21:06:15 -0300

    feat ✨ requirement 1

--- public/script/chat.js
+++ public/script/chat.js
@@ -3,10 +3,13 @@ const socket = window.io();
 const buttonMessage = document.querySelector('.mensagem__btn');
 const inputMessage = document.querySelector('.mensagem__input');
 
+const nick = '';
+
 buttonMessage.addEventListener('click', (event) => {
   event.preventDefault();
   
-  socket.emit('message', { chatMessage: inputMessage.value, nickname: socket.id.slice(1, 17) });
+  const nickname = nick || socket.id.slice(0, 16);
+  socket.emit('message', { chatMessage: inputMessage.value, nickname });
 });
 
 const sendMessage = (message) => {
