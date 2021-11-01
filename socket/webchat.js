b57f0c66723b0b82dd47d43934e88225fe90e4e4
Joao Nasc <oi@joaonasc.dev>
2021-11-01 16:59:40 -0300

    fix: lint

--- socket/webchat.js
+++ socket/webchat.js
@@ -7,30 +7,36 @@ const { create } = require('../models/message');
  * @param {import('socket.io').Server} server
  * @param {Array} users
  */
-// eslint-disable-next-line max-lines-per-function
+function createMessage(nickname, chatMessage) {
+  const currentDate = getCurrentDate().fulldate;
+  const currentTime = getCurrentDate().fulltime;
+  const message = `${currentDate}|${currentTime}|${nickname}|${chatMessage}`;
+  create({
+    message: chatMessage,
+    nickname,
+    timestamp: moment().format('YYYY-MM-DD hh:mm:ss'),
+  });
+  return message;
+}
+
+function createOrUpdateUser(users, socket, nickname) {
+  const index = users.findIndex((user) => user.id === socket.id);
+  if (index >= 0) {
+    users.splice(index, 1, { id: socket.id, nickname });
+  } else {
+    users.push({ id: socket.id, nickname });
+  }
+}
+
 module.exports = (socket, server, users) => {
   socket.on('message', ({ nickname, chatMessage }) => {
-    const currentDate = getCurrentDate().fulldate;
-    const currentTime = getCurrentDate().fulltime;
-    const message = `${currentDate}|${currentTime}|${nickname}|${chatMessage}`;
-    create({
-      message: chatMessage,
-      nickname,
-      timestamp: moment().format('YYYY-MM-DD hh:mm:ss'),
-    });
+    const message = createMessage(nickname, chatMessage);
     server.emit('message', message);
     socket.emit('author-message', message);
   });
 
   socket.on('new-user', (nickname) => {
-    const index = users.findIndex((user) => user.id === socket.id);
-    if (index >= 0) {
-      users.splice(index, 1, { id: socket.id, nickname });
-      console.log('***************************');
-    } else {
-      console.log('///////////////////////////');
-      users.push({ id: socket.id, nickname });
-    }
+    createOrUpdateUser(users, socket, nickname);
     server.emit('show-users', users);
   });
 
