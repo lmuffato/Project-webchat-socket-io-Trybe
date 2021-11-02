3382d573082e45be21d7159e662cb91857963d4a
Elisa <elisafranca.carvalho@gmail.com>
2021-11-01 23:02:12 -0300

    feat: implements 1st and 2nd requirements

---
+++ public/script.js
@@ -0,0 +1,56 @@
+const socket = window.io();
+
+const button = document.querySelector('#sendMsgButton');
+const nickButton = document.querySelector('#sendNickButton');
+const input = document.querySelector('#inputMsg');
+const nickInput = document.querySelector('#inputNickname');
+const messages = document.querySelector('#messageList');
+const clientsList = document.querySelector('#clientsList');
+
+let currentNickname;
+
+socket.on('connect', () => {
+  currentNickname = socket.id;
+});
+
+// Consultei o PR #69 do Iago para auxílio no script.js
+button.addEventListener('click', () => {
+  socket.emit('message', { chatMessage: input.value, nickname: currentNickname });
+  input.value = '';
+  return false;
+});
+
+const createMessage = (message) => {
+  const li = document.createElement('li');
+  li.dataset.testid = 'message';
+  li.innerText = message;
+  messages.appendChild(li);
+};
+
+const createUser = (id) => {
+  const li = document.createElement('li');
+  li.innerText = id;
+  li.setAttribute('data-testid', 'online-user');
+  li.setAttribute('id', id);
+  clientsList.appendChild(li);
+};
+
+const changeNickname = (nickname, id) => {
+  console.log({ id });
+  const nickName = document.querySelector(`#${id}`);
+  console.log('nickName', nickName);
+  nickName.innerText = nickname;
+  currentNickname = nickname;
+};
+
+nickButton.addEventListener('click', () => {
+  socket.emit('nickname', { nickname: nickInput.value, id: socket.id });
+  nickInput.value = '';
+  return false;
+});
+
+socket.on('message', (message) => createMessage(message));
+
+socket.on('nickname', ({ nickname, id }) => changeNickname(nickname, id));
+
+socket.on('user', (id) => createUser(id));
--- server.js
+++ server.js
@@ -1,1 +1,66 @@
-// Faça seu código aqui
+require('dotenv/config');
+
+const express = require('express');
+const moment = require('moment');
+
+// const middlewares = require('./middlewares');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const onlineClients = [];
+
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
+
+app.use(express.static(`${__dirname}/public`));
+
+const options = {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+};
+
+const io = require('socket.io')(http, options);
+
+const getNick = (nickCode) => nickCode.slice(0, 16);
+
+io.on('connection', (socket) => {
+  const nick = getNick(socket.id);
+  const currentClient = { id: nick, nickname: '' };
+
+  socket.emit('user', nick);
+
+  onlineClients.push(currentClient);
+
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const currentNickname = getNick(nickname);
+    const timeStamp = moment(Date.now()).format('DD-MM-yyyy HH:mm:ss');
+    io.emit('message', `${timeStamp} - ${currentNickname}: ${chatMessage}`);
+  });
+
+  socket.on('nickname', (data) => {
+    console.log({ data });
+
+    const nickname = getNick(data.id);
+
+    const clientPosition = onlineClients.findIndex((client) => client.id === nickname);
+    // Fonte: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/splice (sim, até hoje kkk)
+    onlineClients.splice(clientPosition, 1);
+
+    onlineClients.push({ id: nick, nickname: data.nickname });
+    io.emit('nickname', { id: nick, nickname: data.nickname });  
+  });
+});
+
+// app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index', { onlineClients }));
+// app.use(middlewares.error);
+
+http.listen(PORT, () => {
+  console.log(`Ouvindo na porta ${PORT}`);
+});
