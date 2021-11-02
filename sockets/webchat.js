80ba628e7ded2988faaeae7d51cf690262ae68ed
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-02 18:39:22 -0300

    refactor: melhora funçao de disconnect e remove createMessafe de dentro da connection

--- sockets/webchat.js
+++ sockets/webchat.js
@@ -1,16 +1,36 @@
 const moment = require('moment');
-const { addMessage, getMessages } = require('../models/webchat');
+const { 
+    addMessage, 
+    getMessages, 
+    addUser, 
+    deleteUser, 
+    getUsers, 
+    updateNickname } = require('../models/webchat');
+
+    const createMessage = async ({ chatMessage, nickname }, io) => {
+        const dataAtual = moment().format('DD-MM-YYYY HH:mm:ss A');
+        await addMessage({ message: chatMessage, nickname, timestamp: dataAtual });
+        io.emit('message', `${dataAtual} - ${nickname}: ${chatMessage}`);
+    };
 
 module.exports = (io) => 
     io.on('connection', async (socket) => {
-    const messages = await getMessages();
+        // socket.disconnect(0);
+    const messages = (await getMessages())
+    .map(({ message, nickname: nick, timestamp }) => `${timestamp} - ${nick}: ${message}`);
+    const users = addUser(socket.id, socket.id.substring(0, 16));
     socket.emit('userConnected', socket.id);
     socket.emit('messages', messages);
-    
-   socket.on('message', async ({ chatMessage, nickname }) => {
-    const dataAtual = moment().format('DD-MM-YYYY HH:mm:ss A');
-    const message = `${dataAtual} - ${nickname} ${chatMessage}`;
-    await addMessage({ message: chatMessage, nickname, timestamp: dataAtual });
-    io.emit('message', message);
-   }); 
+    io.emit('users', users);
+    socket.on('saveNickname', (nickname) => {
+        const updatedUsers = updateNickname(socket.id, nickname);
+        io.emit('users', updatedUsers);
+    });
+    socket.on('message', async (data) => {
+        await createMessage(data, io);
+    });
+   socket.on('disconnect', () => {
+      deleteUser(socket.id);
+       io.emit('users', getUsers());
+   });
 });
