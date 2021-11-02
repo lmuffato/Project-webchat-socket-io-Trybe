a077f0831672eee3f67f69e201dba7debebfd9c5
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-02 11:45:59 -0300

    feat: configura server

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const expressPort = process.env.PORT || 3000;
+const httpServer = require('http').createServer(app);
+
+const corsOptions = {
+  origin: `http://localhost:${expressPort}`,
+  methods: ['GET', 'POST'],
+};
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    corsOptions,
+  },
+});
+
+const { getMessages } = require('./controllers/webchat');
+
+require('./sockets/webchat')(io);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(cors(corsOptions));
+app.get('/', getMessages);
+
+httpServer.listen(expressPort, () => console.log(`socket rodando na porta ${expressPort}`));
