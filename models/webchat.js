ea644f6aa6efbabe0768c309fe5e3bf8dc0d084e
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-11-01 20:16:26 -0400

    add: function create on model webchat

--- models/webchat.js
+++ models/webchat.js
@@ -1,13 +1,21 @@
 const connection = require('./connection');
 
-const CHATS = 'chats';
+const MESSAGES = 'messages';
 
 const getAll = async () => {
   const db = await connection();
-  const result = await db.collection(CHATS).find({}).toArray();
+  const result = await db.collection(MESSAGES).find({}).toArray();
+  return result;
+};
+
+const create = async (doc) => {
+  const { message, nickname, timestamp } = doc;
+  const db = await connection();
+  const result = await db.collection(MESSAGES).insertOne({ message, nickname, timestamp });
   return result;
 };
 
 module.exports = {
   getAll,
+  create,
 };
