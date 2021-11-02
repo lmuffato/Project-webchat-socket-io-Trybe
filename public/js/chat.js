de0390a4de5ba49116c19ef4769a05373a98a364
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-11-01 22:53:49 -0400

    requirement 2 done

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,49 @@
 const socket = window.io();
 
+const DATA_TESTID = 'data-testid';
+
 window.onbeforeunload = () => {
   socket.disconnect();
 };
 
-const apendOnlineUsers = (users) => {
+const buttonMessage = document.getElementById('send-message');
+const buttonChangeNickname = document.getElementById('nickname-button');
+
+const sendMessage = (message) => {
+  const { nickname } = JSON.parse(sessionStorage.getItem('clientInfo'));
+  socket.emit('message', { nickname, chatMessage: message });
+};
+
+buttonMessage.addEventListener('click', () => {
+  const inputMessage = document.getElementById('input-message');
+  if (!inputMessage.value) {
+    alert('a mensagem não pode ser vazia');
+  } else {
+    sendMessage(inputMessage.value);
+    inputMessage.value = '';
+  }
+});
+
+buttonChangeNickname.addEventListener('click', () => {
+  const nicknameInput = document.getElementById('nickname-input');
+  if (!nicknameInput.value) {
+    alert('o campo nickName não pode ser vazio');
+    return;
+  }
+  console.log('test');
+  const { id } = JSON.parse(sessionStorage.getItem('clientInfo'));
+  socket.emit('changeNickname', { newNickname: nicknameInput.value, id });
+  nicknameInput.value = '';
+});
+
+const appendOnlineUsers = (users) => {
   const listOnlineUsers = document.getElementById('online-users');
   listOnlineUsers.innerHTML = '';
   users.forEach(({ nickname }) => {
     const listItem = document.createElement('li');
 
     listItem.setAttribute('id', nickname);
-    listItem.setAttribute('data-testid', 'online-user');
+    listItem.setAttribute(DATA_TESTID, 'online-user');
 
     listItem.innerText = nickname;
 
@@ -19,6 +51,34 @@ const apendOnlineUsers = (users) => {
   });
 };
 
-socket.on('online-users', (users) => {
-  apendOnlineUsers(users);
+const appendMessage = (message) => {
+  const messageList = document.getElementById('messages');
+  const listItem = document.createElement('li');
+  listItem.setAttribute(DATA_TESTID, 'message');
+  listItem.innerText = message;
+  messageList.appendChild(listItem);
+};
+
+const renderMessages = (messages) => {
+  console.log(messages);
+  const messageList = document.getElementById('messages');
+  messages.forEach((message) => {
+    const listItem = document.createElement('li');
+    listItem.setAttribute(DATA_TESTID, 'message');
+    listItem.innerText = message;
+    messageList.appendChild(listItem);
+  });
+};
+
+socket.on('clientInfo', (client) => {
+  sessionStorage.setItem('clientInfo', JSON.stringify(client));
 });
+
+socket.on('online-users', (users) => {
+  console.log(users);
+  appendOnlineUsers(users);
+});
+
+socket.on('messageHistory', renderMessages);
+
+socket.on('message', appendMessage);
--- sockets/chat.js
+++ sockets/chat.js
@@ -8,32 +8,53 @@ obj message = {
 }
  */
 
+const ONLINE_USERS = 'online-users';
+
+const messagesHistory = [];
+
 const onlineUsers = [];
 
 const findIndexOnlineUser = (socketId) => (
-  onlineUsers.findIndex(({ id }) => socketId.slice(0, 16) === id)
+  onlineUsers.findIndex((user) => socketId === user.id)
 );
 
+const changeNickname = ({ newNickname, id }) => {
+  const index = findIndexOnlineUser(id);
+  onlineUsers[index].nickname = newNickname;
+};
+
+const messageBuilder = ({ nickname, chatMessage }) => {
+  const timestamp = new Date();
+  const message = `${moment(timestamp)
+    .format('DD-MM-yyyy h:mm:ss a')} - ${nickname}: ${chatMessage}`;
+
+  messagesHistory.push(message);
+
+  return message;
+};
+
+const formatNickname = (nickname) => nickname.slice(0, 16);
+
 module.exports = (io) => io.on('connection', (socket) => {
-  console.log(`Usuário conectado. ID: ${socket.id} `);
-  const nick = socket.id.slice(0, 16);
-  onlineUsers.push({ nickname: nick, id: nick });
+  const client = { nickname: formatNickname(socket.id), id: formatNickname(socket.id) };
+  onlineUsers.push(client);
+  socket.emit('clientInfo', client);
+  socket.emit('messageHistory', messagesHistory);
+  io.emit(ONLINE_USERS, onlineUsers);
 
-  io.emit('online-users', onlineUsers);
+  socket.on('message', async (clientInfo) => io.emit('message', messageBuilder(clientInfo)));
 
-  socket.on('message', async (data) => {
-    const { nickname, chatMessage } = data;
-    const timestamp = new Date();
-
-    io.emit('message', `${moment(timestamp)
-      .format('DD-MM-yyyy h:mm:ss a')} - ${nickname}: ${chatMessage}`);
+  socket.on('changeNickname', (nickInfo) => {
+    console.log(onlineUsers);
+    changeNickname(nickInfo);
+    console.log(onlineUsers);
+    io.emit(ONLINE_USERS, onlineUsers);
+    socket.emit('clientInfo', { nickname: nickInfo.newNickname, id: nickInfo.id });
   });
 
   socket.on('disconnect', () => {
-    console.log(`usuário ${socket.id} desconectado`);
-    const index = findIndexOnlineUser(socket.id);
+    const index = findIndexOnlineUser(formatNickname(socket.id));
     onlineUsers.splice(index, 1);
-
-    io.emit('online-users', onlineUsers);
+    io.emit(ONLINE_USERS, onlineUsers);
   });
 });
