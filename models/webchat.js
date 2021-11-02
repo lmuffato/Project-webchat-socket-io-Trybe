eec27157288c7ec86f496b1df94347068bda82f3
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-02 18:37:10 -0300

    refactor: melhora funçao de escrever mensagens e ajusta lista de usuarios

--- models/webchat.js
+++ models/webchat.js
@@ -1,5 +1,7 @@
 const connection = require('./connection');
 
+let users = [];
+
 const addMessage = async (msgData) => {
     const db = await connection();
     await db.collection('messages').insertOne({ ...msgData });
@@ -10,4 +12,26 @@ const getMessages = async () => {
     return db.collection('messages').find().toArray();
 };
 
-module.exports = { addMessage, getMessages };
+const addUser = (id, nickname) => {
+    users.push({ id, nickname });
+    return users;
+};
+
+const getUsers = () => users;
+
+const deleteUser = (id) => {
+   users = users.filter((user) => id !== user.id);
+};
+
+const updateNickname = (id, nickname) => {
+    const updatedUsers = users.map((user) => {
+        if (user.id === id) {
+            return ({ id, nickname });
+        }
+        return user;
+    });
+    users = updatedUsers;
+    return updatedUsers;
+};
+
+module.exports = { addMessage, getMessages, addUser, deleteUser, getUsers, updateNickname };
